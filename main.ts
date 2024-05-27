a2a2ea3330990066643cd820aa309b763f4421bf
Narjis Fatima <161502591+NarjisFatima62@users.noreply.github.com>
2024-05-27 23:02:57 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,12 @@
+//Question 22:
+//Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+
+//Answer:
+
+const fruit: String[] = ["Apple", "Pineapple", "Cherry", "Banana", "Grapes", "Blueberry","Mango"];
+//console.log(fruit{7});
+
+//Now,
+
+console.log(fruit[5]);
+
